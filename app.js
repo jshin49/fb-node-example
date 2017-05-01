9d95b2829ceddba78ca9cef2279f1e54f73df919
Jay Shin <jshin49@gmail.com>
2017-05-01 16:15:13 +0800

    init

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ const
   https = require('https'),  
   request = require('request');
 
+var app = express();
+app.set('port', process.env.PORT || 5000);
+
+/*
+ * Use your own validation token. Check that the token used in the Webhook 
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
