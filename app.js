3a1fc12787616c33498ec38475396b147e1b0c25
Jay Shin <jshin49@gmail.com>
2017-05-01 16:51:19 +0800

    Basic webhook verification

--- app.js
+++ app.js
@@ -9,20 +9,101 @@ const
   request = require('request');
 
 var app = express();
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.set('port', process.env.PORT || 5000);
 
+
 /*
- * Use your own validation token. Check that the token used in the Webhook 
+ * Be sure to setup your config values before running this code. You can 
+ * set them using environment variables or modifying the config file in /config.
+ *
+ */
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = 
+  (process.env.APP_SECRET) ? 
+  process.env.APP_SECRET :
+  config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VERIFY_TOKEN = 
+  (process.env.VERIFY_TOKEN) ?
+  process.env.VERIFY_TOKEN :
+  config.get('verifyToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = 
+  (process.env.PAGE_ACCESS_TOKEN) ?
+  process.env.PAGE_ACCESS_TOKEN :
+  config.get('pageAccessToken');
+
+// URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const PAGE_ID = 
+  (process.env.PAGE_ID) ?
+  process.env.PAGE_ID :
+  config.get('pageID');
+
+if (!(APP_SECRET && VERIFY_TOKEN && PAGE_ACCESS_TOKEN && PAGE_ID)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
+
+/*
+ * Use your own verify token. Check that the token used in the Webhook 
  * setup is the same token used here.
  *
  */
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log("Validating webhook");
+      req.query['hub.verify_token'] === VERIFY_TOKEN) {
+    console.log("Verifying webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
+    console.error("Failed verification. Make sure the verify tokens match.");
     res.sendStatus(403);          
   }  
 });
+
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from 
+ * the App Dashboard, we can verify the signature that is sent with each 
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an 
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid 
+// certificate authority.
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
