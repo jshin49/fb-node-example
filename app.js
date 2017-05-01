3a8d3b4316953c29b07d0bb175e38ea03e73c905
Jay Shin <jshin49@gmail.com>
2017-05-01 15:54:24 +0800

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const 
+  bodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),  
+  request = require('request');
+
