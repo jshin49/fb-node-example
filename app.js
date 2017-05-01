45af0f802f5fe8bf21ca3f477e7aa27e855eb229
Jay Shin <jshin49@gmail.com>
2017-05-01 17:43:17 +0800

    Basic echoing chatbot finished

--- app.js
+++ app.js
@@ -76,43 +76,259 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
-  console.log(data);
-  res.sendStatus(200);
   // Make sure this is a page subscription
-  // if (data.object == 'page') {
-  //   // Iterate over each entry
-  //   // There may be multiple if batched
-  //   data.entry.forEach(function(pageEntry) {
-  //     var pageID = pageEntry.id;
-  //     var timeOfEvent = pageEntry.time;
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        console.log(messagingEvent)
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
 
-  //     // Iterate over each messaging event
-  //     pageEntry.messaging.forEach(function(messagingEvent) {
-  //       if (messagingEvent.optin) {
-  //         receivedAuthentication(messagingEvent);
-  //       } else if (messagingEvent.message) {
-  //         receivedMessage(messagingEvent);
-  //       } else if (messagingEvent.delivery) {
-  //         receivedDeliveryConfirmation(messagingEvent);
-  //       } else if (messagingEvent.postback) {
-  //         receivedPostback(messagingEvent);
-  //       } else if (messagingEvent.read) {
-  //         receivedMessageRead(messagingEvent);
-  //       } else if (messagingEvent.account_linking) {
-  //         receivedAccountLink(messagingEvent);
-  //       } else {
-  //         console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-  //       }
-  //     });
-  //   });
-
-  //   // Assume all went well.
-  //   //
-  //   // You must send back a 200, within 20 seconds, to let us know you've 
-  //   // successfully received the callback. Otherwise, the request will time out.
-  //   res.sendStatus(200);
-  // }
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've 
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
 });
+
+
+
+/*
+ * Authorization Event
+ *
+ * The value for 'optin.ref' is defined in the entry point. For the "Send to 
+ * Messenger" plugin, it is the 'data-ref' field. Read more at 
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/authentication
+ *
+ */
+function receivedAuthentication(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfAuth = event.timestamp;
+
+  // The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
+  // The developer can set this to an arbitrary value to associate the 
+  // authentication callback with the 'Send to Messenger' click event. This is
+  // a way to do account linking when the user clicks the 'Send to Messenger' 
+  // plugin.
+  var passThroughParam = event.optin.ref;
+
+  console.log("Received authentication for user %d and page %d with pass " +
+    "through param '%s' at %d", senderID, recipientID, passThroughParam, 
+    timeOfAuth);
+
+  // When an authentication is received, we'll send a message back to the sender
+  // to let them know it was successful.
+  sendTextMessage(senderID, "Authentication successful");
+}
+
+
+/*
+ * Message Event
+ *
+ * This event is called when a message is sent to your page. The 'message' 
+ * object format can vary depending on the kind of message that was received.
+ * Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+ *
+ * For this example, we're going to echo any text that we get. If we get some 
+ * special keywords ('button', 'generic', 'receipt'), then we'll send back
+ * examples of those bubbles to illustrate the special message bubbles we've 
+ * created. If we receive a message with an attachment (image, video, audio), 
+ * then we'll simply confirm that we've received the attachment.
+ * 
+ */
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var isEcho = message.is_echo;
+  var messageId = message.mid;
+  var appId = message.app_id;
+  var metadata = message.metadata;
+
+  // You may get a text or attachment but not both
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+  var quickReply = message.quick_reply;
+
+  if (isEcho) {
+    // Just logging message echoes to console
+    console.log("Received echo for message %s and app %d with metadata %s", 
+      messageId, appId, metadata);
+    return;
+  } else if (quickReply) {
+    var quickReplyPayload = quickReply.payload;
+    console.log("Quick reply for message %s with payload %s",
+      messageId, quickReplyPayload);
+
+    sendTextMessage(senderID, "Quick reply tapped");
+    return;
+  }
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches any special
+    // keywords and send back the corresponding example. Otherwise, just echo
+    // the text we received.
+    switch (messageText) {
+      // case 'image':
+      //   sendImageMessage(senderID);
+      //   break;
+
+      // case 'gif':
+      //   sendGifMessage(senderID);
+      //   break;
+
+      // case 'audio':
+      //   sendAudioMessage(senderID);
+      //   break;
+
+      // case 'video':
+      //   sendVideoMessage(senderID);
+      //   break;
+
+      // case 'file':
+      //   sendFileMessage(senderID);
+      //   break;
+
+      // case 'button':
+      //   sendButtonMessage(senderID);
+      //   break;
+
+      // case 'generic':
+      //   sendGenericMessage(senderID);
+      //   break;
+
+      // case 'receipt':
+      //   sendReceiptMessage(senderID);
+      //   break;
+
+      // case 'quick reply':
+      //   sendQuickReply(senderID);
+      //   break;        
+
+      // case 'read receipt':
+      //   sendReadReceipt(senderID);
+      //   break;        
+
+      // case 'typing on':
+      //   sendTypingOn(senderID);
+      //   break;        
+
+      // case 'typing off':
+      //   sendTypingOff(senderID);
+      //   break;        
+
+      // case 'account linking':
+      //   sendAccountLinking(senderID);
+      //   break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+
+/*
+ * Delivery Confirmation Event
+ *
+ * This event is sent to confirm the delivery of a message. Read more about 
+ * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
+ *
+ */
+function receivedDeliveryConfirmation(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var delivery = event.delivery;
+  var messageIDs = delivery.mids;
+  var watermark = delivery.watermark;
+  var sequenceNumber = delivery.seq;
+
+  if (messageIDs) {
+    messageIDs.forEach(function(messageID) {
+      console.log("Received delivery confirmation for message ID: %s", 
+        messageID);
+    });
+  }
+
+  console.log("All message before %d were delivered.", watermark);
+}
+
+
+/*
+ * Send a text message using the Send API.
+ *
+ */
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText,
+      metadata: "DEVELOPER_DEFINED_METADATA"
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll 
+ * get the message id in a response 
+ *
+ */
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s", 
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });  
+}
 
 
 /*
